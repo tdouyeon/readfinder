acb20aca8f4942926c48371f637553aa54e6740c
tdouyeon <xhzhdy@naver.com>
2025-01-29 19:18:55 +0900

    suspense 추가

--- src/app/detail/page.tsx
+++ src/app/detail/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useSearchParams } from "next/navigation";
+import React, { Suspense } from "react";
 
 export default function DetailPage() {
   const searchParams = useSearchParams();
@@ -12,39 +13,41 @@ export default function DetailPage() {
   };
 
   return (
-    <div
-      className="flex justify-center items-center min-h-screen p-5"
-      style={{ backgroundImage: "url('/book.jpg')" }}
-    >
-      <div className="bg-white p-12 rounded-lg w-full max-w-3xl ">
-        <h1 className="text-4xl font-extrabold text-gray-800 mb-6 text-center">
-          {book.title}
-        </h1>
+    <Suspense fallback={<div>로딩중...</div>}>
+      <div
+        className="flex justify-center items-center min-h-screen p-5"
+        style={{ backgroundImage: "url('/book.jpg')" }}
+      >
+        <div className="bg-white p-12 rounded-lg w-full max-w-3xl ">
+          <h1 className="text-4xl font-extrabold text-gray-800 mb-6 text-center">
+            {book.title}
+          </h1>
 
-        <div>
-          <span className="text-gray-600">분류 | </span>
-          {book.category}
-        </div>
+          <div>
+            <span className="text-gray-600">분류 | </span>
+            {book.category}
+          </div>
 
-        <div className="mb-4">
-          <span className="text-gray-600">작가 | </span>
-          <span className=" text-gray-800">{book.rights}</span>
-        </div>
+          <div className="mb-4">
+            <span className="text-gray-600">작가 | </span>
+            <span className=" text-gray-800">{book.rights}</span>
+          </div>
 
-        <div className="mb-6">
-          <span className="text-gray-600">설명 | </span>
-          <p className=" text-gray-700 mt-2">{book.description}</p>
-        </div>
+          <div className="mb-6">
+            <span className="text-gray-600">설명 | </span>
+            <p className=" text-gray-700 mt-2">{book.description}</p>
+          </div>
 
-        <div className="flex justify-center mt-6">
-          <button
-            onClick={() => window.history.back()}
-            className="bg-[#2d6b60] text-white px-8 py-3 mt-10 rounded-lg hover:bg-[#1f4a43] transition transform hover:scale-105 leading-relaxed"
-          >
-            뒤로가기
-          </button>
+          <div className="flex justify-center mt-6">
+            <button
+              onClick={() => window.history.back()}
+              className="bg-[#2d6b60] text-white px-8 py-3 mt-10 rounded-lg hover:bg-[#1f4a43] transition transform hover:scale-105 leading-relaxed"
+            >
+              뒤로가기
+            </button>
+          </div>
         </div>
       </div>
-    </div>
+    </Suspense>
   );
 }
